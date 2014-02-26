2d5fd68f5168741a2109dcb6c4c12c3f33740933
Paul <pleichty@iastate.edu>
2014-02-26 15:02:07 -0600

    added properties to student model

--- app/models/student.py
+++ app/models/student.py
@@ -1,17 +1,31 @@
 from google.appengine.ext import db
 
-
 # You can find more out about types and properties of google app engine 
 # database models here! 
 # https://developers.google.com/appengine/docs/python/datastore/typesandpropertyclasses
 class Student(db.Model):
-    name = db.StringProperty(required=True)
     age = db.IntegerProperty(required=True)
+	gpa = db.FloatProperty(required=True)
+	major = db.StringProperty(required=True)
+	department = db.StringPropperty(required=True)
+	gender = db.BooleanProperty(required=True)
+	homestate = db.StringProperty(required=True)
 
+def fetch_students_by_gpa(gpa):
+	return Student.all().filter('gpa', gpa).fetch(limit=None)
 
+def fetch_students_by_department(department):
+	return Student.all().filter('department', department).fetch(limit=None)
+	
+def fetch_students_by_gender(gender):
+	return Student.all().filter('gender', gender).fetch(limit=None)
+		
 def fetch_students_by_age(age):
     return Student.all().filter('age', age).fetch(limit=None)
 
+def fetch_students_by_homestate(homestate):
+	return Student.all().filter('homestate', homestate).fetch(limit=None)
+	
 def fetch_students(limit=100, offset=0):
     return Student.all().fetch(limit=limit, offset=offset)
 
