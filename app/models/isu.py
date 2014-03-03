cbaeb446e4f2d557c78fa79ff7ffd39b8255c2a1
Robert Sheehy <gameboy1092@gmail.com>
2014-03-03 16:24:53 -0600

    Prettify our home page/chart page/template
    Also added a python file that loads all, or
    at least most, of Iowa State’s colleges and
    majors into memory for graphing usage.

---
+++ app/models/isu.py
@@ -0,0 +1,167 @@
+agriculture = [
+    'Environmental Science',
+    'Agricultural Biochemistry',
+    'Environmental Studies',
+    'Agricultural Business',
+    'Food Science',
+    'Agricultural Studies',
+    'Forestry',
+    'Agricultural Systems Technology',
+    'Genetics',
+    'Agriculture Exploration',
+    'Global Resource Systems',
+    'Agronomy',
+    'Horticulture',
+    'Animal Ecology',
+    'Industrial Technology',
+    'Animal Science',
+    'International Agriculture',
+    'Biology',
+    'Microbiology',
+    'Culinary Science',
+    'Nutritional Science',
+    'Dairy Science',
+    'Public Service and Administration in Agriculture',
+    'Diet and Exercise',
+    'Seed Science',
+    'Dietetics',
+]
+
+
+engineering = [
+    'Aerospace Engineering',
+    'Agricultural Engineering',
+    'Biosystems Engineering',
+    'Chemical Engineering',
+    'Biological Engineering',
+    'Civil Engineering',
+    'Construction Engineering',
+    'Environmental Engineering',
+    'Electrical Engineering',
+    'Computer Engineering',
+    'Industrial Engineering',
+    'Manufacturing Systems Engineering',
+    'Materials Science and Engineering',
+    'Mechanical Engineering'
+]
+
+business = [
+    'Accounting',
+    'Business Economics',
+    'Finance',
+    'Marketing',
+    'Management',
+    'Management Information Systems',
+    'Supply Chain Management'
+]
+
+vet = [
+    'Biomedical Sciences',
+    'Veterinary Clinical Sciences',
+    'Veterinary Diagnostic and Production Animal Medicine',
+    'Veterinary Microbiology and Preventive Medicine',
+    'Veterinary Pathology'
+]
+
+design = [
+    'Architecture',
+    'Art and Design',
+    'Community and Regional Planning',
+    'Design',
+    'Graphic Design',
+    'Industrial Design',
+    'Integrated Studio Arts',
+    'Interior Design',
+    'Landscape Architecture'
+]
+
+
+
+human = [
+    'Apparel, Merchandising, and Design',
+    'Family and Consumer Sciences Ed',
+    'Athletic Training',
+    'Financial Counseling and Planning',
+    'Child, Adult, and Family Services',
+    'Food Science',
+    'Culinary Science',
+    'Hospitality Management',
+    'Kinesiology and Health',
+    'Dietetics',
+    'Nutritional Science',
+    'Early Childhood Education',
+    'Elementary Education',
+    'Entrepreneurial Studies',
+    'Event Management'
+]
+
+
+
+liberal = [
+    'Advertising',
+    'African and African American Studies',
+    'American Indian Studies',
+    'Astronomy',
+    'Anthropology',
+    'Asian American Studies',
+    'Biochemistry',
+    'Bioinformatics and Computational Biology',
+    'Biological and Pre-Medical Illustration',
+    'Biology',
+    'Biophysics',
+    'Chemistry',
+    'Classical Studies',
+    'Communication Studies',
+    'Computer Science',
+    'Criminal Justice',
+    'Earth Science',
+    'Economics',
+    'English',
+    'Environmental Science & Studies',
+    'French',
+    'Genetics',
+    'Geology and Earth Science',
+    'German',
+    'History',
+    'Intercultural Studies',
+    'International Studies',
+    'Journalism and Mass Communication',
+    'Liberal Studies',
+    'Linguistics',
+    'Mathematics',
+    'Meteorology',
+    'Military Studies',
+    'Music',
+    'Open Option',
+    'Performing Arts',
+    'Philosophy',
+    'Physics',
+    'Political Science',
+    'Pre-Health Programs',
+    'Pre Law',
+    'Psychology',
+    'Religious Studies',
+    'Sociology',
+    'Software Engineering',
+    'Spanish',
+    'Speech Communication',
+    'Statistics',
+    'Technical Communication',
+    'Teaching Certification',
+    'Technology and Social Change',
+    'Women\'s Studies',
+    'World Languages and Cultures'
+]
+
+
+colleges = {
+    'Agriculture and Life Sciences': agriculture,
+    'Engineering': engineering,
+    'Liberal Arts and Sciences': liberal,
+    'Business': business,
+    'Veterinary Medicine': vet,
+    'Design': design,
+    'Human Sciences': human
+}
+
+
